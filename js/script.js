b65457b631197e36424f555630f6b9835dd604eb
Oskar <ostrowskioskar@gmail.com>
2022-07-28 22:49:36 +0200

    styling

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const API_LINK_GEOCODING = 'http://api.openweathermap.org/geo/1.0/direct?q='
 const API_UNITS = '&units=metric'
 
 const getWeather = () => {
-    const city = input.value || 'Warszawa'
+    const city = input.value || 'Poznan'
     const URL = API_LINK_GEOCODING + city + '&limit=1' + API_KEY
 
     axios.get(URL)
@@ -21,15 +21,45 @@ const getWeather = () => {
             const lat = res.data[0].lat
             const lon = res.data[0].lon
             const URL = API_LINK_WEATHER + lat + '&lon=' + lon + API_KEY + API_UNITS
+
             axios.get(URL)
                 .then(res => {
                     weather.textContent = res.data.weather[0].main
-                    temperature.textContent = Math.floor(res.data.main.temp)
-                    humidity.textContent = res.data.main.humidity
-                })
-        })
+                    temperature.textContent = Math.floor(res.data.main.temp) + '℃'
+                    humidity.textContent = res.data.main.humidity + '%'
 
-    cityName.textContent = input.value
+                    const status = res.data.weather[0].id
+
+                    if(status >= 200 && status <= 232) {
+                        photo.setAttribute('src', './img/thunderstorm.png')
+                    } else if (status >= 300 && status <= 321) {
+                        photo.setAttribute('src', './img/shower_rain.png')
+                    } else if (status >= 500 && status <= 504) {
+                        photo.setAttribute('src', './img/rain.png')
+                    } else if (status === 511) {
+                        photo.setAttribute('src', './img/snow.png')
+                    } else if (status > 511 && status <= 531) {
+                        photo.setAttribute('src', './img/shower_rain.png')
+                    } else if (status >= 600 && status <= 622) {
+                        photo.setAttribute('src', './img/snow.png')
+                    } else if (status >= 701 && status <= 781) {
+                        photo.setAttribute('src', './img/mist.png')
+                    } else if (status === 800) {
+                        photo.setAttribute('src', './img/clear.png')
+                    } else if (status === 801) {
+                        photo.setAttribute('src', './img/few_clouds.png')
+                    } else if (status === 802) {
+                        photo.setAttribute('src', './img/scattered_clouds.png')
+                    } else if (status === 803 || status === 804) {
+                        photo.setAttribute('src', './img/broken_clouds.png')
+                    }
+                })
+                .catch(err => console.error(err))
+        })
+        .catch(err => console.error(err))
+
+    cityName.textContent = input.value || 'Poznan'
 }
 
+getWeather()
 button.addEventListener('click', getWeather)
