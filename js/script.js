5310210ca53b2a4809fb81505d256a0204a7335f
Oskar <ostrowskioskar@gmail.com>
2022-08-02 18:37:48 +0200

    finding user city working

--- js/script.js
+++ js/script.js
@@ -19,76 +19,80 @@ const API_UNITS = '&units=metric'
 
 let theme = '#2B2B2B'
 
+const getCords = (lat, lon) => {
+    const URL = `${API_LINK_WEATHER}lat=${lat}&lon=${lon}${API_KEY}${API_UNITS}`
+
+    axios.get(URL)
+        .then(res => {
+            cityName.textContent = res.data.name
+                    
+            weather.textContent = res.data.weather[0].main
+            temperature.textContent = Math.floor(res.data.main.temp) + '℃'
+            humidity.textContent = res.data.main.humidity + '%'
+
+            warning.textContent = ''
+            input.style.border = ''
+
+            const status = res.data.weather[0].id
+
+            if(status >= 200 && status <= 232) {
+                photo.setAttribute('src', './img/thunderstorm.png')
+            } else if (status >= 300 && status <= 321) {
+                photo.setAttribute('src', './img/shower_rain.png')
+            } else if (status >= 500 && status <= 504) {
+                photo.setAttribute('src', './img/rain.png')
+            } else if (status === 511) {
+                photo.setAttribute('src', './img/snow.png')
+            } else if (status > 511 && status <= 531) {
+                photo.setAttribute('src', './img/shower_rain.png')
+            } else if (status >= 600 && status <= 622) {
+                photo.setAttribute('src', './img/snow.png')
+            } else if (status >= 701 && status <= 781) {
+                photo.setAttribute('src', './img/mist.png')
+            } else if (status === 800) {
+                photo.setAttribute('src', './img/clear.png')
+            } else if (status === 801) {
+                photo.setAttribute('src', './img/few_clouds.png')
+            } else if (status === 802) {
+                photo.setAttribute('src', './img/scattered_clouds.png')
+            } else if (status === 803 || status === 804) {
+                photo.setAttribute('src', './img/broken_clouds.png')
+            }
+        })
+        .catch(err => console.error(err))
+} 
+
 const getWeather = () => {
     const city = input.value || 'Poznan'
     const URL = API_LINK_GEOCODING + city + '&limit=1' + API_KEY
 
     axios.get(URL)
         .then(res => {
-            cityName.textContent = res.data[0].name
-
             const lat = res.data[0].lat
             const lon = res.data[0].lon
 
-            const URL = `${API_LINK_WEATHER}lat=${lat}&lon=${lon}${API_KEY}${API_UNITS}`
-
-            axios.get(URL)
-                .then(res => {
-                    weather.textContent = res.data.weather[0].main
-                    temperature.textContent = Math.floor(res.data.main.temp) + '℃'
-                    humidity.textContent = res.data.main.humidity + '%'
-
-                    const status = res.data.weather[0].id
-
-                    if(status >= 200 && status <= 232) {
-                        photo.setAttribute('src', './img/thunderstorm.png')
-                    } else if (status >= 300 && status <= 321) {
-                        photo.setAttribute('src', './img/shower_rain.png')
-                    } else if (status >= 500 && status <= 504) {
-                        photo.setAttribute('src', './img/rain.png')
-                    } else if (status === 511) {
-                        photo.setAttribute('src', './img/snow.png')
-                    } else if (status > 511 && status <= 531) {
-                        photo.setAttribute('src', './img/shower_rain.png')
-                    } else if (status >= 600 && status <= 622) {
-                        photo.setAttribute('src', './img/snow.png')
-                    } else if (status >= 701 && status <= 781) {
-                        photo.setAttribute('src', './img/mist.png')
-                    } else if (status === 800) {
-                        photo.setAttribute('src', './img/clear.png')
-                    } else if (status === 801) {
-                        photo.setAttribute('src', './img/few_clouds.png')
-                    } else if (status === 802) {
-                        photo.setAttribute('src', './img/scattered_clouds.png')
-                    } else if (status === 803 || status === 804) {
-                        photo.setAttribute('src', './img/broken_clouds.png')
-                    }
-                })
-                .catch(err => console.error(err))
+            getCords(lat, lon)
         })
         .catch(err => {
             console.error(err)
-            warning.textContent = 'Error'
+            warning.textContent = 'City not found'
+            input.style.border = '1px solid #ed4337'
         })
 
         input.value = ''
 }
 
-function geoFindMe() {
+const geoFindMe = () => {
     if (!navigator.geolocation){
         console.log("Geolocation is not supported by your browser")
         return
     }
 
     const success = position => {
-        const lat  = position.coords.latitude
-        const lon = position.coords.longitude
-
-        const URL = `${API_LINK_REV_GEOCODING}${lat}&lon=${lon}${API_KEY}`
-
-        axios.get(URL)
-            .then(res => console.log(res.data[0].name))
-            .catch(err => console.log(err))
+        const lat  = position.coords.latitude;
+        const lon = position.coords.longitude;
+        
+        getCords(lat, lon)
     }
 
     return navigator.geolocation.getCurrentPosition(success, () => {
